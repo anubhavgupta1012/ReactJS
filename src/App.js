4e0a1c50a5bbcca3d244b87204ce92f698a89c26
anubhavgupta1012 <anubhavgupta1012@gamil.com>
2020-08-01 23:58:44 +0530

    Component created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react';
+
+class App extends Component{
+    render(){
+        return(
+                    <div>
+                    <h1>Hi , This is the start of you are making billionaire</h1>
+                    <p> This is the way you will learn things.</p>
+
+                    <p> Just keep Leaning</p>
+                    </div>
+        )
+         }
+}
+export default App;
