c2d7f6b23d1566250acb45eb7a13a2310dd01da7
anubhavgupta1012 <anubhavgupta1012@gamil.com>
2020-08-03 21:47:14 +0530

    state changed manually

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React ,{Component} from 'react';
 
 class App extends Component{
+constructor(){
+    super();
+this.state={ displayBio : true}
+}
+    // render(){
+    //     let decs =this.state.displayBio?( 
+    //         <div>
+    //         <p>Hi This is Anubhav</p>
+    //         <p>Java is my Tech </p>
+    //         </div>):null;
+    //     return(
+    //                 <div>
+    //                 <h1>Hi , This is the start of you are making billionaire</h1>
+    //                 <p> This is the way you will learn things.</p>
+    //                 {decs}
+    //                 <p> Just keep Leaning</p>
+    //                 </div>
+    //     )
+    //      }
+
     render(){
         return(
                     <div>
                     <h1>Hi , This is the start of you are making billionaire</h1>
                     <p> This is the way you will learn things.</p>
-
+                    {
+                        this.state.displayBio?( 
+                            <div>
+                            <p>Hi This is Anubhav</p>
+                            <p>Java is my Tech </p>
+                            </div>):null
+                    }
                     <p> Just keep Leaning</p>
                     </div>
         )
          }
+         
 }
 export default App;
