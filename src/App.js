deab4fddd77ca3d79f9bddc6956480b940d8f563
anubhavgupta1012 <anubhavgupta1012@gamil.com>
2020-08-06 21:32:00 +0530

    able to change the state properties

--- src/App.js
+++ src/App.js
@@ -3,37 +3,31 @@ import React ,{Component} from 'react';
 class App extends Component{
 constructor(){
     super();
-this.state={ displayBio : true}
+this.state={ displayBio : false }
+this.displayBioToggler = this.displayBioToggler.bind(this);
 }
-    // render(){
-    //     let decs =this.state.displayBio?( 
-    //         <div>
-    //         <p>Hi This is Anubhav</p>
-    //         <p>Java is my Tech </p>
-    //         </div>):null;
-    //     return(
-    //                 <div>
-    //                 <h1>Hi , This is the start of you are making billionaire</h1>
-    //                 <p> This is the way you will learn things.</p>
-    //                 {decs}
-    //                 <p> Just keep Leaning</p>
-    //                 </div>
-    //     )
-    //      }
 
-    render(){
+displayBioToggler(){
+    this.setState({displayBio :!this.state.displayBio});
+}
+
+render(){
         return(
                     <div>
-                    <h1>Hi , This is the start of you are making billionaire</h1>
-                    <p> This is the way you will learn things.</p>
+                    <h1>Hi , This is Anubhav.</h1>
+                    <p> I am a software Engineer.</p>
+                    <p>I currently work for Daffodil Softwares. </p>
                     {
                         this.state.displayBio?( 
                             <div>
-                            <p>Hi This is Anubhav</p>
+                            <p>I am 23 years old.</p>
                             <p>Java is my Tech </p>
-                            </div>):null
+                            <p>I need to learn the things. </p>
+                            <button onClick = {this.displayBioToggler}>Read Less</button>
+                            </div>):(
+                            <button onClick = {this.displayBioToggler}>Read More</button>                            
+                            )
                     }
-                    <p> Just keep Leaning</p>
                     </div>
         )
          }
